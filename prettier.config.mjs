28f4279ac87aaa49479e4c9c56a112be62c2f950
Bora Baloglu <baloglub@gmail.com>
2025-01-26 04:20:53 +0000

    feat(*): initial commit

---
+++ prettier.config.mjs
@@ -0,0 +1,12 @@
+/** @type {import('prettier').Config} */
+const config = {
+  endOfLine: "lf",
+  semi: false,
+  singleQuote: false,
+  tabWidth: 2,
+  trailingComma: "es5",
+  plugins: ["@ianvs/prettier-plugin-sort-imports"],
+  importOrderParserPlugins: ["typescript", "jsx", "decorators-legacy"],
+};
+
+export default config;
